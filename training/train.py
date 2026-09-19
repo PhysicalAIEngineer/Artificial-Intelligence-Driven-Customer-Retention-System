b98ac09d13544a913a59c319082b8d3be9e91bc9
Chetan Sonigara <chetansonigara01@gmail.com>
2026-09-19 21:08:38 +0530

    Match Ruff import classification

--- training/train.py
+++ training/train.py
@@ -7,10 +7,10 @@ from datetime import datetime, timezone
 from pathlib import Path
 
 import joblib
-import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 
+import mlflow
 from src.data import load_dataframe, prepare_xy
 from src.evaluation import (
     classification_metrics,
