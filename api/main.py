9ea1ef089d4690b1922dfe0ae6588513316e244b
Chetan Sonigara <chetansonigara01@gmail.com>
2026-09-19 21:07:56 +0530

    Remove unused API database import

--- api/main.py
+++ api/main.py
@@ -13,7 +13,6 @@ from fastapi import FastAPI, Header, HTTPException
 from prometheus_client import Counter, Gauge, Histogram, make_asgi_app
 from pydantic import BaseModel, Field
 from sqlalchemy import create_engine, text
-from sqlalchemy.exc import SQLAlchemyError
 
 ROOT = Path(__file__).resolve().parents[1]
 ARTIFACT_DIR = Path(
